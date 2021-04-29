726105ce8f9810178b212831691ee5d36257333a
fairuz-tarannum <83390485+fairuz-tarannum@users.noreply.github.com>
2021-04-30 00:26:35 +0600

    Add files via upload

---
+++ controller/logout.php
@@ -0,0 +1,11 @@
+<?php 
+require_once('include/config.php');
+session_start();
+require_once('include/config.php');
+if(isset($_SESSION['is_logged_in'])){
+    unset($_SESSION['is_logged_in']);
+    unset($_SESSION['admin_data']);
+    session_destroy();
+}
+header('Location: '.ROOT_URL.'controller/login.php');
+
